d1ea7329400ff728ad0af57af38b814c590fd8f3
AngelesCornejo <cornejoa969@gmail.com>
2021-05-19 19:12:40 -0500

    cambios

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+let rootPath = path.normalize(__dirname + '/Public');
+
+app.set('port',process.env.PORT || 5000);
+
+app.use(express.static('./Public'));
+
+
+app.get('/', (req,res)=>{
+    //res.send("Hello world");
+   res.sendFile(rootPath + '/views/login.html');
+ });
+
+ app.listen(app.get('port'),()=>{
+     console.log("Server running")
+    });
