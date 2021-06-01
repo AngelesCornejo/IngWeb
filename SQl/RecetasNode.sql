c819e1dcbbe5a48155a7bddea7ddbccf2d58a399
Josue-Arciniega <josue.arcinieganoriega@gmail.com>
2021-06-01 13:06:05 -0500

    Agrega cambios en DB

--- SQl/RecetasNode.sql
+++ SQl/RecetasNode.sql
@@ -7,14 +7,23 @@ create table usuarios(id_user int NOT NULL AUTO_INCREMENT,
 nombre varchar(25),
 password varchar(120),
 email varchar(25),
+estrellas_prom double(3,2),
+npuntuaciones int,
 primary key(id_user)
 );
 
 
+create table categorias(
+id_categoria int auto_increment Primary key,
+categoria varchar(20) not null
+);
+
 
 create table ingredientes(id_ingr int NOT NULL AUTO_INCREMENT,
 nomb_ing varchar(25),
-primary key(id_ingr)
+id_categoria int not null,
+primary key(id_ingr),
+FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
 );
 
 create table recetas(
@@ -27,7 +36,9 @@ nacionalidad varchar(30),
 calorias int,
 porcion_calorias int,
 tipo_porcion int,
-primary key(id_receta)
+id_categoria int not null,
+primary key(id_receta),
+FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
 );
 
 create table req_especiales(
@@ -53,27 +64,25 @@ FOREIGN KEY (id_receta) REFERENCES recetas(id_receta),
 FOREIGN KEY (id_ingr) REFERENCES ingredientes(id_ingr)
 );
 
+insert into categorias(categoria) values("Todos"),("Puerco"),("Pollo");
 
-insert into ingredientes(nomb_ing) values ("Zanahoria");
-insert into ingredientes(nomb_ing) values ("Papa");
-insert into ingredientes(nomb_ing) values ("Aceite");
-insert into ingredientes(nomb_ing) values ("Calabaza");
-insert into ingredientes(nomb_ing) values ("Huevo");
-insert into ingredientes(nomb_ing) values ("Tortillas");
-insert into ingredientes(nomb_ing) values ("Jitomate");
-insert into ingredientes(nomb_ing) values ("Cebolla");
-insert into ingredientes(nomb_ing) values ("Chile de arbol");
-insert into ingredientes(nomb_ing) values ("Chicharos");
+
+insert into ingredientes(id_categoria, nomb_ing) values (1,"Zanahoria");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Papa");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Aceite de olivo");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Calabaza");
+insert into ingredientes(id_categoria,nomb_ing) values (3,"Huevo");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Tortillas");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Jitomate");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Cebolla");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Chile de arbol");
+insert into ingredientes(id_categoria,nomb_ing) values (1,"Chicharos");
 
 insert into req_especiales(req) values ("Horno de gas"),("Horno de microondas"),("Horno electrico"),("Estufa"),("Batidora"),("Licuadora");
 
 
 
-insert into recetas(nb_imagen_receta, nomb_receta, descripcion,tiempo,nacionalidad,calorias,porcion_calorias,tipo_porcion) 
-values ("Pollo 1.png","Pollo asado","Un pollito bien asadito",15, "internacional",300,100,2),
-<<<<<<< HEAD
-values ("MexEgg.png","MexicanEgg","Un huevo a la mexicana",15, "Mexico",650,1,1);
-=======
- ("MexEgg.png","MexicanEgg","Un huevo a la mexicana",15, "Mexico",650,1,1);
+insert into recetas(nb_imagen_receta, nomb_receta, descripcion,tiempo,nacionalidad,calorias,porcion_calorias,tipo_porcion,id_categoria) 
+values ("Pollo 1.png","Pollo asado","Un pollito bien asadito",15, "internacional",300,100,2,3),
+ ("MexEgg.png","MexicanEgg","Un huevo a la mexicana",15, "Mexico",650,1,1,3);
 insert into req_rec values(4,1,0),(4,2,0);
->>>>>>> 0f47fd15f89e334c188713d943f7f05547bf6f50
